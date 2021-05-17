45297530353b129f4dc5ddcc01957bfff6b05518
brandonamos <bpamos21@gmail.com>
2021-05-17 12:36:52 -0600

    (WIP) adding blob and creating public container

--- azurerm_storage_blob.tf
+++ azurerm_storage_blob.tf
@@ -2,16 +2,16 @@
 
 
 
-# resource "azurerm_storage_container" "mycontainer" {
-#   name                  = "content"
-#   storage_account_name  = azurerm_storage_account.mystorageaccount.name
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_container" "mycontainer" {
+  name                  = "benchmarkoutput"
+  storage_account_name  = azurerm_storage_account.mystorageaccount.name
+  container_access_type = "container"        #"private"
+}
 
-# resource "azurerm_storage_blob" "myblob" {
-#   name                   = "memtier_outfile.json"
-#   storage_account_name   = azurerm_storage_account.mystorageaccount.name
-#   storage_container_name = azurerm_storage_container.mycontainer.name
-#   type                   = "Block"
-#   # source                 = "memtier_outfile.json"
-# }
+resource "azurerm_storage_blob" "myblob" {
+  name                   = "memtier_outfile.json"
+  storage_account_name   = azurerm_storage_account.mystorageaccount.name
+  storage_container_name = azurerm_storage_container.mycontainer.name
+  type                   = "Block"
+  # source                 = "memtier_outfile.json"
+}
